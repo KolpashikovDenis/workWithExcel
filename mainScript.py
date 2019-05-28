ce643e3bd572b20f94aecd02433453a305a1db1d
Denis <kolpashikov.denis@gmail.com>
2019-05-28 23:32:04 +0400

    некоторые исправления

--- mainScript.py
+++ mainScript.py
@@ -1,32 +1,51 @@
-import openpyxl
+from datetime import datetime
+from openpyxl import Workbook
+from openpyxl.writer.excel import save_workbook
 
-def space_to_tab(line):
+def space_to_tab(_line):
     c = []
     i = 0
-    while i < len(line):
-        if line[i] == ' ':
+    while i < len(_line):
+        if _line[i] == ' ':
             c.append('\t')
-            while line[i] == ' ':
+            while _line[i] == ' ':
                 i += 1
         else:
-            c.append(line[i])
+            c.append(_line[i])
         i += 1
 
-    line = ''.join(c)
-    return line
+    _line = ''.join(c)
+    return _line
 
 
-f_name = 'sar_mpgu_izh.csv'
+#========================= MAIN CODE  =====================================================================
+# Формируем имя файла
+f_name = "Report_%Y-%m-%d_%H-%M-%S"
+print(datetime.now().strptime())
+
+# делаем екселевский файл
+Items = ['Graphs', 'CPU', 'MEM', 'DISK', 'NET']
+wb_report = Workbook()
+for item in Items:
+    wb_report.create_sheet(item)
 
 
-with open(f_name, 'r') as rdfile:
-    while True:
-        line = rdfile.readline().strip()
-        if line == '':
-            rdfile.readline().strip()
-            break
 
-    while True:
-        line = rdfile.readline().strip()
-        space_to_tab(line)
-        print(line)
+#
+#
+# f_name = 'sar_mpgu_izh.csv'
+#
+# with open(f_name, 'r') as rdfile:
+#     while True:
+#         line = rdfile.readline().strip()
+#         if line == '':
+#             rdfile.readline().strip()
+#             break
+#
+#     while True:
+#         line = rdfile.readline().strip()
+#         if line == '':
+#             break
+#
+#         space_to_tab(line)
+#         print(line)
