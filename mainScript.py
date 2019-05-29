bc65dc61c8afc9dace4c25e3b3747184bcb1ac79
KolpashikovDenis <KolpashikovDenis@bitbucket.org>
2019-05-29 15:03:31 +0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	mainScript.py

--- mainScript.py
+++ mainScript.py
@@ -22,8 +22,7 @@ def space_to_tab(line):
 
 #=========   MAIN CODE ===============================
 # Формируем имя файла
-f_name = "Report_%Y-%m-%d_%H-%M-%S"
-print(datetime.now().strptime())
+f_name = 'Report_'+datetime.strftime(datetime.now(), "%Y-%m-%d_%H-%M-%S")+'.xlsx'
 
 data = []
 book = Workbook()
@@ -31,29 +30,44 @@ book = Workbook()
 # делаем екселевский файл
 Items = ['Graphs', 'CPU', 'MEM', 'DISK', 'NET']
 wb_report = Workbook()
+
 for item in Items:
     wb_report.create_sheet(item)
+wdel = wb_report['Sheet']
+wb_report.remove(wdel)
+wb_report.save(f_name)
 
-with open(f_name, 'r') as rdfile:
+with open('sar_mpgu_izh.csv', 'r') as rdfile:
+    # Пропустим первые две строки
     while True:
         line = rdfile.readline().strip()
         if line == '':
-            line = rdfile.readline().strip()
-            c = line.split('\t')
             break
 
+    # Заполняем страничку с данными по CPU
+    active_sheet = wb_report[Items[1]]
+
+    _row = 1
     while True:
         line = rdfile.readline().strip()
-        if line == '':
+        if (line == '') or ('Average' in line):
             break
         if 'all' in line:
-            data.append(space_to_tab(line).split('\t'))
+            #data.append(space_to_tab(line).split('\t'))
+            data = space_to_tab(line).split(('\t'))
+            data.remove('all')
             # TODO: чтение выхлапа sar, создание первой диаграммы
-            #print(data)
+            for i in range(1, len(data)+1):
+                active_sheet.cell(row = _row, column = i).value = data[i - 1]
+            _row += 1
+
         else:
             continue
 
+    wb_report.save(f_name)
     print('----------------------------------------------------------------------------')
+
+    active_sheet = wb_report[Items[2]]
 
     while True:
         line = rdfile.readline().strip()
@@ -61,7 +75,7 @@ with open(f_name, 'r') as rdfile:
             break
         if 'all' in line:
             data = space_to_tab(line).split('\t')
-            # TODO: чтение выхлапа sar, создание первой диаграммы
+            # TODO: чтение выхлапа sar, создание второй диаграммы
             print(data)
         else:
             continue
