5c5ef97f545c16a15ff4a57c8dea0325ea92ffd6
KolpashikovDenis <KolpashikovDenis@bitbucket.org>
2019-05-29 08:59:02 +0400

    Есть немного изменений

--- mainScript.py
+++ mainScript.py
@@ -1,4 +1,4 @@
-import openpyxl
+from openpyxl import Workbook
 
 def space_to_tab(line):
     c = []
@@ -8,6 +8,7 @@ def space_to_tab(line):
             c.append('\t')
             while line[i] == ' ':
                 i += 1
+            c.append(line[i])
         else:
             c.append(line[i])
         i += 1
@@ -16,17 +17,57 @@ def space_to_tab(line):
     return line
 
 
+
+#=========   MAIN CODE ===============================
 f_name = 'sar_mpgu_izh.csv'
+
+data = []
+book = Workbook()
 
 
 with open(f_name, 'r') as rdfile:
     while True:
         line = rdfile.readline().strip()
         if line == '':
-            rdfile.readline().strip()
+            line = rdfile.readline().strip()
+            c = line.split('\t')
             break
 
     while True:
         line = rdfile.readline().strip()
-        space_to_tab(line)
-        print(line)
+        if line == '':
+            break
+
+        if 'all' in line:
+            data.append(space_to_tab(line).split('\t'))
+            # TODO: чтение выхлапа sar, создание первой диаграммы
+            #print(data)
+        else:
+            continue
+
+    print('----------------------------------------------------------------------------')
+
+    while True:
+        line = rdfile.readline().strip()
+        if line == '':
+            break
+        if 'all' in line:
+            data = space_to_tab(line).split('\t')
+            # TODO: чтение выхлапа sar, создание первой диаграммы
+            print(data)
+        else:
+            continue
+
+    print('----------------------------------------------------------------------------')
+
+    while True:
+        line = rdfile.readline().strip()
+        if line == '':
+            break
+        if 'all' in line:
+            data = space_to_tab(line).split('\t')
+            # TODO: чтение выхлапа sar, создание первой диаграммы
+            print(data)
+        else:
+            continue
+
