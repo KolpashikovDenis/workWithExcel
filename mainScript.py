11b7418439f563bf6716230f5df0e01cd42ef065
KolpashikovDenis <KolpashikovDenis@bitbucket.org>
2019-06-18 09:38:21 +0400

    теперь в данные пишет не стринги а флоаты

--- mainScript.py
+++ mainScript.py
@@ -20,6 +20,28 @@ def space_to_tab(line):
     line = ''.join(c)
     return line
 
+def show_util_cpu(t, cpu, qcpu):
+    np_x = np.asarray(t)
+    np_y1 = np.asarray(cpu)
+    np_y2 = np.asarray(q_cpu)
+
+    fig, ax = plt.subplots()
+    ax.plot(np_x, np_y1, color='blue', label='Утилизация CPU')
+    ax.plot(np_x, np_y2, color='green', label='Очередь CPU')
+    ax.set_xlabel('Продолжительность теста, ч:мм')
+    ax.set_ylabel('Утилизация CPU, %')
+    ax.set_title('Утилизация CPU')
+    plt.legend(loc='upper left')
+    ylim = ax.get_ylim()
+
+    sub_ax = ax.twinx()
+    sub_ax.plot([], [])
+    sub_ax.set_ylabel('Очереди CPU, шт')
+    sub_ax.set_ylim(ylim[0], ylim[1])
+    plt.grid()
+
+    plt.show()
+
 
 #=========   MAIN CODE ===============================
 # Формируем имя файла
@@ -30,6 +52,7 @@ f_name_with_data = "sar_mpgu_izh.csv"
 data = []
 cpu = []
 q_cpu = []
+t = []
 
 # делаем екселевский файл
 Items = ['Graphs', 'CPU', 'MEM', 'DISK', 'NET', 'LOAD_AVG']
@@ -59,6 +82,7 @@ with open(f_name_with_data, 'r') as infile:
         if '%idle' in line:
             data = space_to_tab(line).split('\t')
             del data[1:10]
+            data[0] = data[0][:5]
             active_sheet.cell(row = 1, column = 1).value = data[0]
             active_sheet.cell(row = 1, column = 2).value = data[1]
             break
@@ -71,11 +95,13 @@ with open(f_name_with_data, 'r') as infile:
         if 'all' in line:
             data = space_to_tab(line).split('\t')
             del data[1:10]
+            data[0] = data[0][:5]
             active_sheet.cell(row = r, column = 1).value = data[0]
-            active_sheet.cell(row = r, column = 2).value = float(data[1].replace(',', '.'))
-            cpu.append(float(data[1].replace(',', '.')))
+            t.append(data[0])
+            a = 100.0 - float(data[1].replace(',', '.'))
+            active_sheet.cell(row = r, column = 2).value = a
+            cpu.append(a)
             r += 1
-
 
     # очереди CPU
     while True:
@@ -93,8 +119,13 @@ with open(f_name_with_data, 'r') as infile:
             break
         data = space_to_tab(line).split('\t')
         del data[2:]
-        active_sheet.cell(row = r, column = 3).value = float(data[1].replace(',', '.'))
+        a = float(data[1].replace(',', '.'))
+        active_sheet.cell(row = r, column = 3).value = a
+        q_cpu.append(a)
         r += 1
+
+#Рисуем график 'Утилизация CPU' и 'Очереди CPU'
+show_util_cpu(t, cpu, q_cpu)
 
 # Данные для МЕМ
 with open(f_name_with_data, 'r') as infile:
@@ -105,6 +136,7 @@ with open(f_name_with_data, 'r') as infile:
             data = space_to_tab(line).split('\t')
             del data[1:3]
             del data[2:]
+            data[0] = data[0][:5]
             active_sheet.cell(row = 1, column = 1).value = data[0]
             active_sheet.cell(row = 1, column = 2).value = data[1]
             break
@@ -117,6 +149,7 @@ with open(f_name_with_data, 'r') as infile:
         data = space_to_tab(line).split('\t')
         del data[1:3]
         del data[2:]
+        data[0] = data[0][:5]
         active_sheet.cell(row = r, column = 1).value = data[0]
         active_sheet.cell(row = r, column = 2).value = float(data[1].replace(',', '.'))
         r += 1
@@ -149,6 +182,7 @@ with open(f_name_with_data, 'r') as infile:
         if 'DEV' in line:
             data = space_to_tab(line).split('\t')
             del data[1:6]
+            data[0] = data[0][:5]
             active_sheet.cell(row = 1, column = 1).value = data[0]
             active_sheet.cell(row = 1, column = 2).value = data[1]
             active_sheet.cell(row = 1, column = 3).value = data[2]
@@ -164,6 +198,7 @@ with open(f_name_with_data, 'r') as infile:
             break
         data = space_to_tab(line).split('\t')
         del data[2:6]
+        data[0] = data[0][:5]
         key = data[0]
         del data[0:2]
         if key in map.keys():
@@ -172,7 +207,7 @@ with open(f_name_with_data, 'r') as infile:
             map[key] = [data]
 
 
-    size = len(map['09:21:06'])
+    size = len(map['09:21'])
     avg_map = {}
     rowNum = 2
     for key in map.keys():
@@ -203,6 +238,7 @@ with open(f_name_with_data, 'r') as infile:
             data = space_to_tab(line).split('\t')
             del data[1:4]
             del data[3:]
+            data[0] = data[0][:5]
             active_sheet.cell(row=1, column=1).value = data[0]
             active_sheet.cell(row=1, column=2).value = data[1]
             active_sheet.cell(row=1, column=3).value = data[2]
@@ -216,6 +252,7 @@ with open(f_name_with_data, 'r') as infile:
         data = space_to_tab(line).split('\t')
         del data[1:4]
         del data[3:]
+        data[0] = data[0][:5]
         key = data[0]
         del data[0]
         if key in map.keys():
@@ -223,7 +260,7 @@ with open(f_name_with_data, 'r') as infile:
         else:
             map[key] = [data]
 
-    size = len(map['09:21:06'])
+    size = len(map['09:21'])
     avg_map = {}
     rowNum = 2
     for key in map.keys():
@@ -246,6 +283,7 @@ with open(f_name_with_data, 'r') as infile:
         if 'runq-sz' in line:
             data = space_to_tab(line).split('\t')
             del data[1:3]
+            data[0] = data[0][:5]
             active_sheet.cell(row = 1, column = 1).value = data[0]
             active_sheet.cell(row = 1, column = 2).value = data[1]
             active_sheet.cell(row = 1, column = 3).value = data[2]
@@ -259,6 +297,7 @@ with open(f_name_with_data, 'r') as infile:
             break
         data = space_to_tab(line).split('\t')
         del data[1:3]
+        data[0] = data[0][:5]
         active_sheet.cell(row=r, column=1).value = data[0]
         active_sheet.cell(row=r, column=2).value = float(data[1].replace(',', '.'))
         active_sheet.cell(row=r, column=3).value = float(data[2].replace(',', '.'))
